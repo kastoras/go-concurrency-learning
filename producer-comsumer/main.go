97cf417a2eccd943d2f536a3a56d5157699eecad
nikolaosanthimidis <nikolaos.anthimidis@visma.com>
2023-07-03 13:50:41 +0300

    End of producer consumer example

--- producer-comsumer/main.go
+++ producer-comsumer/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 
-	"github.com/labstack/gommon/color"
+	"github.com/fatih/color"
 )
 
 const NumberOfPizzas = 10
@@ -28,15 +29,72 @@ func (p *Producer) Close() error {
 	return <-ch
 }
 
+func makePizza(pizzaNumber int) *PizzaOrder {
+	pizzaNumber++
+	if pizzaNumber <= NumberOfPizzas {
+
+		delay := rand.Intn(5) + 1
+		fmt.Printf("Received order #%d!\n", pizzaNumber)
+
+		rnd := rand.Intn(12) + 1
+		msg := ""
+		success := false
+
+		if rnd < 5 {
+			pizzasFailed++
+		} else {
+			pizzasMade++
+		}
+		total++
+
+		fmt.Printf("Making pizza #%d. It will take %d seconds...\n", pizzaNumber, delay)
+		//delay
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2 {
+			msg = fmt.Sprintf("** We ran out od ingrediands for pizza #%d!", pizzaNumber)
+		} else if rnd <= 4 {
+			msg = fmt.Sprintf("** The cook quite while pizza #%d!", pizzaNumber)
+		} else {
+			success = true
+			msg = fmt.Sprintf("Pizza order #%d is ready", pizzaNumber)
+		}
+
+		p := PizzaOrder{
+			pizzaNumber: pizzaNumber,
+			message:     msg,
+			success:     success,
+		}
+
+		return &p
+
+	}
+
+	return &PizzaOrder{
+		pizzaNumber: pizzaNumber,
+	}
+}
+
 func pizzeria(pizzaMaker *Producer) {
 	// keep track of whick pizza we are making
+	var i = 0
 
 	// run forever or until we reveive a quit notification
 	// try to make pizza
-
 	for {
-		//try to make a pizza
-		// decision
+		currentPizza := makePizza(i)
+		if currentPizza != nil {
+			i = currentPizza.pizzaNumber
+			select {
+			// we tried to make a pizza (we sent something to the data channel)
+			case pizzaMaker.data <- *currentPizza:
+
+			case quitChan := <-pizzaMaker.quit:
+				close(pizzaMaker.data)
+				close(quitChan)
+				return
+			}
+		}
 	}
 
 }
@@ -59,6 +117,28 @@ func main() {
 	go pizzeria(pizzaJob)
 
 	// create and run consumer
+	for i := range pizzaJob.data {
+		if i.pizzaNumber <= NumberOfPizzas {
+			if i.success {
+				color.Green(i.message)
+				color.Green("Order #%d is out for delivery!", i.pizzaNumber)
+			} else {
+				color.Red(i.message)
+				color.Red("Order #%d have been failed to complete....", i.pizzaNumber)
+			}
+
+		} else {
+			color.Cyan("Done making pizzas...")
+			err := pizzaJob.Close()
+			if err != nil {
+				color.Red("*** Error closing channel!", err)
+			}
+		}
+	}
 
 	//printout the ending message
+	color.Cyan("---------------")
+	color.Cyan("Done for the day.")
+
+	color.Cyan("%d pizzas madem failed to make %d with %d attemts", pizzasMade, pizzasFailed, total)
 }
